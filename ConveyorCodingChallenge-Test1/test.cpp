3af2497161094447c206927c9648ae4d29aa54ca
Mike Davies <mike-gitlab@mikedavies.net>
2021-12-06 13:24:15 +0000

    First (non-working) version of Test.cpp

--- ConveyorCodingChallenge-Test1/test.cpp
+++ ConveyorCodingChallenge-Test1/test.cpp
@@ -1,6 +1,22 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "..\ConveyorCodingChallenge\SimBase.h"
+#include "..\ConveyorCodingChallenge\Worker.h"
+#include "..\ConveyorCodingChallenge\ItemType.h"
+
+#include <iostream>
+
+TEST(TestCaseName, TestName)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(WorkerTest, MTTest)
+{
+//	Worker w1;
+	ItemType item_in = ItemType::MT;
+//	ItemType item_out = w1.time_step(item_in);
+
+//	EXPECT_EQ(item_in, item_out);
 }
